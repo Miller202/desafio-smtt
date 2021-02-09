f0b97021d0267f33e8d510d48388072fe663d5b3
Michael Miller <mullerg4michael@gmail.com>
2021-02-09 10:29:24 -0300

    Visualizações sobre atrasos, adiantamentos e pontualidades no trânsito;
    - Atrasos por empresa;
    - Adiantamentos por empresa;
    - Percentual de pontualidade por empresa.

---
+++ src/Empresa_adiantamento.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('viagens.csv')
+
+data1 = data.empresa[data.duracao_prevista > data.duracao_realizada]
+data2 = pd.DataFrame(data1) # check the dataframe.
+table = pd.DataFrame(data1.value_counts()) # get the frequencies in the frequency table.
+
+x = table.index #empresa
+y = table.empresa #frequencia de adiantamentos
+
+#visual
+plt.figure(figsize=(16,8))
+plt.title('Quantidade de adiantamentos por empresa', fontsize=20)
+plt.xlabel('Empresa', fontsize=15)
+plt.ylabel('Adiantamentos', fontsize=15)
+plt.grid(axis='y', color='#a3a3a3', linestyle="--")
+plt.ylim((0, 4500))
+
+#plot
+plt.bar(x, y, color='#42f57e', edgecolor='#a3a3a3', width=0.8)
+plt.show()
---
+++ src/Empresa_pontualidade.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('viagens.csv')
+
+data1 = data.empresa[(data.duracao_prevista == data.duracao_realizada) 
+  | (data.duracao_realizada == (data.duracao_prevista + 1.0)) 
+  | (data.duracao_realizada == (data.duracao_prevista - 1.0))
+  | (data.duracao_realizada == (data.duracao_prevista + 2.0)) 
+  | (data.duracao_realizada == (data.duracao_prevista - 2.0))]
+
+data2 = pd.DataFrame(data1) # check the dataframe.
+table = pd.DataFrame(data1.value_counts()) # get the frequencies in the frequency table.
+
+x = table.index #empresa
+y = table.empresa #frequencia de adiantamentos
+explode = (0.1, 0, 0, 0)
+
+# plot chart
+plt.rcParams['font.size'] = 14.0
+fig1, ax1 = plt.subplots(figsize=(16,8))
+ax1.pie(values, explode=explode, labels=label, autopct='%1.1f%%',
+        shadow=True, startangle=75)
+ax1.axis('equal')
+plt.title("Percentual de pontualidade das viagens por empresa", fontsize=20, bbox={'facecolor':'0.8', 'pad':5})
+plt.show()
+
